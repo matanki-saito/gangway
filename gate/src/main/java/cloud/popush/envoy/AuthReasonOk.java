821585e98140625665e89604fcfad84d2eb760b5
matanki.saito <matanki.saito@gmail.com>
2023-09-02 12:27:14 +0900

    [app build] Add disocrd notify

--- gate/src/main/java/cloud/popush/envoy/AuthReasonOk.java
+++ gate/src/main/java/cloud/popush/envoy/AuthReasonOk.java
@@ -3,6 +3,6 @@ package cloud.popush.envoy;
 public class AuthReasonOk extends AuthResult {
     public AuthReasonOk() {
         this.reason = "Ok";
-        this.isOk = false;
+        this.isOk = true;
     }
 }
